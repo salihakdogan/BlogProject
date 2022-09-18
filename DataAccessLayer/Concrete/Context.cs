9e6726b43b256ca61401daf8fe8bdc0a801dfd59
salihakdogan <salihakdogan1@gmail.com>
2022-09-18 23:42:57 +0300

    Update class, create new references with other projects

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,11 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("server=LAPTOP-M3NQRT96; database=BlogProject; integrated security=true;");
         }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
