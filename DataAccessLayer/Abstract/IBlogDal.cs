17c51c6570fa64f9e5b88a73f6f356e0ea5131e3
salihakdogan <salihakdogan1@gmail.com>
2022-09-18 23:56:37 +0300

    Add new interfaces

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal
+    {
+        List<Blog> ListBlog();
+        void AddBlog(Blog blog);
+        void RemoBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetBlog(int id);
+    }
+}
---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> ListCategory();
+        void AddCategory(Category category);
+        void RemoveCategory(Category category);
+        void UpdateCategory(Category category);
+        Category GetCategory(int id);
+    }
+}
