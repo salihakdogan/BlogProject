a7796a6d89cc9e8118cf056f1673cd75ab18212d
salihakdogan <salihakdogan1@gmail.com>
2022-09-23 00:17:37 +0300

    Add new controller and partial views, update view

---
+++ BlogProject/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AddCommentToBlog()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult CommentsOnTheBlog()
+        {
+            return PartialView();
+        }
+    }
+}
